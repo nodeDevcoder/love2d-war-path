a151117ba4c62d54701434770ef0dcea9cb5508b
nodeDevcoder <jibrilasif@gmail.com>
2021-05-20 12:44:23 +0530

    first commit

---
+++ main.lua
@@ -0,0 +1,27 @@
+
+Class = require 'class'
+
+require 'states/Start'
+
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+function love.load()   
+    math.randomseed(os.time())
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.window.setTitle('My Game')
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
+end
+
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    -- love.graphics.setBackgroundColor(1, 1, 1)
+    -- local width, height = love.graphics.getDimensions()
+    -- love.graphics.setScissor(0, 0, width / 2, height)
+
+end
