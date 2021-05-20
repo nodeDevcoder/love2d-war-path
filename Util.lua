196962ca6d5476340c34838e3281ba92c3b88ace
nodeDevcoder <jibrilasif@gmail.com>
2021-05-20 13:44:28 +0530

    add player keys

---
+++ Util.lua
@@ -0,0 +1,5 @@
+
+player_keys = {}
+
+player_keys[1] = { up = 'w', down = 's', left = 'a', right = 'd' }
+player_keys[2] = { up = 'up', down = 'down', left = 'left', right = 'right' }
--- states/Start.lua
+++ states/Start.lua
@@ -1,14 +1,12 @@
 Start = Class{}
 
-
 function Start:init()
     self.players = 2
-    
 end
 
 
 function Start:update(dt)
-
+    
 end
 
 function Start:render()
