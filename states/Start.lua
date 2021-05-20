860d3745ab98c719059bff4e1159ba61ed493076
nodeDevcoder <jibrilasif@gmail.com>
2021-05-20 16:59:28 +0530

    add top left bands on start screen

--- states/Start.lua
+++ states/Start.lua
@@ -23,4 +23,9 @@ function Start:render()
     love.graphics.setBackgroundColor(204 / 255, 98 / 255, 22 / 255)
     love.graphics.setFont(fonts.lovelo.large)
     love.graphics.printf('Welcome to War Path!', 0, 100, WINDOW_WIDTH, 'center')
+
+    love.graphics.setLineWidth(10)
+    love.graphics.line(-5, 150, 150, -5) -- top left band1 (-5 to cover empty sides)
+    love.graphics.line(-5, 200, 200, -5) -- top left band2 (-5 to cover empty sides)
+    -- love.graphics.setBackgroundColor(1, 1, 1)
 end
